1978c08380cdf44ca27955644f1829e52002c6d8
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-07 20:11:51 -0300

    Update create_diarization_dataset_librispeech.py

--- scripts/speaker_tasks/create_diarization_dataset_librispeech.py
+++ scripts/speaker_tasks/create_diarization_dataset_librispeech.py
@@ -79,7 +79,7 @@ def load_speaker_sample(speaker_lists, speaker_turn):
 def create_new_entry(new_file, start, speaker_id):
     end = start + new_file['duration']
     #return [start,end,speaker_id]
-    return str(start)+','+str(end)+','+str(speaker_id)
+    return str(start)+' '+str(end)+' '+str(speaker_id)
 
 def main(
     input_manifest_filepath, output_rttm_filepath, output_wav_filepath
