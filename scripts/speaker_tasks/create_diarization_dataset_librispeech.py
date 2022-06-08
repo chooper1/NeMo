5e3f9bb54ad3d310b9c88cb5ea804c920a14612b
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-08 01:20:34 -0300

    Update create_diarization_dataset_librispeech.py

--- scripts/speaker_tasks/create_diarization_dataset_librispeech.py
+++ scripts/speaker_tasks/create_diarization_dataset_librispeech.py
@@ -16,27 +16,17 @@ import argparse
 import json
 import os
 import random
-import wave
 
+import wave
+from filelist_to_manifest import read_manifest #TODO add support for multiple input manifest files?
 # from nemo.collections.asr.parts.preprocessing.segment import AudioSegment
 from nemo.collections.asr.parts.utils.speaker_utils import labels_to_rttmfile
-from filelist_to_manifest import read_manifest
 
 random.seed(42)
 
 """
 This script creates a synthetic diarization dataset using the LibriSpeech dataset.
 """
-
-#read manifest file, from NeMo/examples/nlp/token_classification/punctuate_capitalize_infer.py
-#TODO add support for multiple input manifest files
-# def read_manifest(manifest_path):
-#     manifest_data = []
-#     with open(manifest_path, 'r', encoding='utf-8') as f:
-#         for line in f:
-#             data = json.loads(line)
-#             manifest_data.append(data)
-#     return manifest_data
 
 #randomly select 2 speaker ids from loaded dict
 #TODO make parameterizable
@@ -55,6 +45,7 @@ def get_speaker_ids(list):
 
 #get a list of the samples for the two specified speakers
 #TODO enforce exclusion within one clip (avoid repetition)?
+#TODO replace with more efficient sampling method?
 def get_speaker_samples(file_list, speaker_ids):
     speaker_lists = {'sp1': [], 'sp2': []}
     for file in file_list:
@@ -79,76 +70,56 @@ def load_speaker_sample(speaker_lists, speaker_turn):
 #add new entry to dict (to write to output manifest file)
 def create_new_entry(new_file, start, speaker_id):
     end = start + new_file['duration']
-    #return [start,end,speaker_id]
-    return str(start)+' '+str(end)+' '+str(speaker_id)
+    return str(start) + ' ' + str(end) + ' ' + str(speaker_id)
 
 def main(
-    input_manifest_filepath, output_rttm_filepath, output_wav_filepath
+    input_manifest_filepath, output_dir, output_filename = 'librispeech_diarization', session_length = 600
 ):
-    # if os.path.exists(output_rttm_filepath):
-    #     os.remove(output_rttm_filepath)
-    # if os.path.exists(output_wav_filepath):
-    #     os.remove(output_wav_filepath)
+    if os.path.exists(output_dir):
+        os.remove(output_dir)
+    os.mkdir(output_dir)
 
     #load librispeech manifest file
     input_file = read_manifest(input_manifest_filepath)
 
     #get speaker ids for a given diarization session
     speaker_ids = get_speaker_ids(input_file) #randomly select 2 speaker ids
+    speaker_lists = get_speaker_samples(input_file, speaker_ids) #get list of samples per speaker
 
-    #build list of samples for speaker1 / speaker2
-    #TODO replace with more efficient sampling method
-    speaker_lists = get_speaker_samples(input_file, speaker_ids)
-
-    session_length = 60*10 #initially assuming 10 minute meeting length
+    speaker_turn = 0 #assume alternating between speakers 1 & 2
     running_length = 0
+    manifest_list = []
 
-    #assume alternating between speakers 1 & 2
-    speaker_turn = 0
-
-    #TODO assuming no silence padding, no overlap
-    current_dict = []
-
-    outfile = 'librispeech_diarization_0001'
-
-    wavpath = os.path.join(output_wav_filepath, outfile+'.wav')
+    wavpath = os.path.join(output_dir, output_filename+'.wav')
     with wave.open(wavpath, 'wb') as wav_out:
 
         while (running_length < session_length):
-            #randomly sample from each speaker (TODO enforce exclusion)
             file = load_speaker_sample(speaker_lists, speaker_turn)
             filepath = file['audio_filepath']
 
-            #load wav file
-            #new_wav = AudioSegment.from_file(filepath, target_sr=16000)
-
+            #TODO fixed size wav before loop?
             with wave.open(filepath, 'rb') as wav_in:
                 if not wav_out.getnframes():
                     wav_out.setparams(wav_in.getparams())
                 wav_out.writeframes(wav_in.readframes(wav_in.getnframes()))
 
-            #build dict as you go
             #TODO fixed size dict before loop?
             new_entry = create_new_entry(file, running_length, speaker_ids[speaker_turn])
-            current_dict.append(new_entry)
+            manifest_list.append(new_entry)
 
-            #update speaker turn
             speaker_turn = (speaker_turn + 1) % 2
             running_length += file['duration']
 
     wav_out.close()
-    #write manifest file
-    labels_to_rttmfile(current_dict, outfile, output_rttm_filepath)
+    labels_to_rttmfile(manifest_list, output_filename, output_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input_manifest_filepath", help="path to input manifest file", type=str, required=True)
-    parser.add_argument("--output_rttm_filepath", help="path to output manifest file", type=str, required=True)
-    parser.add_argument("--output_wav_filepath", help="path to output wav files", type=str, required=True)
+    parser.add_argument("--output_dir", help="path to output directory", type=str, required=True)
     args = parser.parse_args()
 
     main(
         args.input_manifest_filepath,
-        args.output_rttm_filepath,
-        args.output_wav_filepath
+        args.output_dir,
     )
