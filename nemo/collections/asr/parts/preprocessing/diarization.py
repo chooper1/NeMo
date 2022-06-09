848290aa133838cf3691411322927decf6bcc2ea
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-08 21:16:52 -0300

    Update diarization.py

--- nemo/collections/asr/parts/preprocessing/diarization.py
+++ nemo/collections/asr/parts/preprocessing/diarization.py
@@ -146,8 +146,10 @@ class LibriSpeechGenerator(object):
             print(length)
             print(array.shape)
             print(audio_file.shape)
-            if (start+length > self._session_length):
-                np.pad(array, pad_width=(0, start+length-self._session_length), mode='constant')
+            print(self._session_length*self._sr)
+            if (start+length > self._session_length*self._sr):
+                print('pad')
+                np.pad(array, pad_width=(0, start+length-self._session_length*self._sr), mode='constant')
             array[start:start+length] = audio_file[:length]
 
             new_entry = self.create_new_rttm_entry(file, running_length, speaker_ids[speaker_turn])
