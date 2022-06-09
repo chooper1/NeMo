9b9ccd789439dd20de6bb8d6d0c738033ed241b1
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-08 21:19:52 -0300

    Update diarization.py

--- nemo/collections/asr/parts/preprocessing/diarization.py
+++ nemo/collections/asr/parts/preprocessing/diarization.py
@@ -142,17 +142,10 @@ class LibriSpeechGenerator(object):
             length = int(duration*self._sr)
 
             # Remove once frame-level word alignments are available?
-            print(start)
-            print(length)
-            print(array.shape)
-            print(audio_file.shape)
-            print(self._session_length*self._sr)
             if (start+length > self._session_length*self._sr):
-                print('pad')
-                print(start+length-self._session_length*self._sr)
                 array = np.pad(array, pad_width=(0, start+length-self._session_length*self._sr), mode='constant')
-            print(array.shape)
-            print(audio_file.shape)
+            print(self._session_length*self._sr)
+            print(start+length)
             array[start:start+length] = audio_file[:length]
 
             new_entry = self.create_new_rttm_entry(file, running_length, speaker_ids[speaker_turn])
