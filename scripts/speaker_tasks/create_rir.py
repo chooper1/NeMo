b862399e5e85a208e287b09506cbb8fa5bd4c3a8
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-09 14:42:01 -0300

    Update create_rir.py

--- scripts/speaker_tasks/create_rir.py
+++ scripts/speaker_tasks/create_rir.py
@@ -43,7 +43,7 @@ def main():
     orV_rcv = None # Vectors pointing in the same direction than the receivers (None assumes omnidirectional)
     mic_pattern = "omni" # Receiver polar pattern
     abs_weights = [0.9]*5+[0.5] # Absortion coefficient ratios of the walls
-    T60 = 16	 # Time for the RIR to reach 60dB of attenuation [s]
+    T60 = 1	 # Time for the RIR to reach 60dB of attenuation [s]
     att_diff = 15.0	# Attenuation when start using the diffuse reverberation model [dB]
     att_max = 60.0 # Attenuation at the end of the simulation [dB]
     fs=16000.0 # Sampling frequency [Hz]
@@ -62,7 +62,7 @@ def main():
 
     speaker_id = 0
     print(RIR[0,speaker_id,:len(input_wav)].shape)
-    output_sound=convolve(input_wav,RIR[speaker_id,:len(input_wav)])
+    output_sound=convolve(input_wav,RIR[0,speaker_id,:len(input_wav)])
     output_sound=output_sound/np.max(np.abs(output_sound)) #normalize to [-1,1]
     print(output_sound)
     sf.write("output.wav", output_sound, fs)
