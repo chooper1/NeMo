fad81959c5d632afba68234633ab11b8e26497f9
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-10 15:55:52 -0300

    Update create_alignment_manifest.py

--- scripts/speaker_tasks/create_alignment_manifest.py
+++ scripts/speaker_tasks/create_alignment_manifest.py
@@ -56,6 +56,7 @@ def get_unaligned_examples(unaligned_path, dataset):
 
             print(unaligned_dataset)
             print(dataset)
+            print(unaligned_dataset == dataset)
             i+=1
 
     return skip_files
