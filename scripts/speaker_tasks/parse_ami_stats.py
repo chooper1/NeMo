aa82f5bc5499ecd941d6c1bae1cb083e2bc95742
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-15 13:09:39 -0300

    Stats

--- scripts/speaker_tasks/parse_ami_stats.py
+++ scripts/speaker_tasks/parse_ami_stats.py
@@ -39,6 +39,8 @@ def main():
     input_directory = args.input_directory
     list = read_cmi_files(input_directory)
 
+    sentence_break_time = 1.0 #1 second
+
     for key,meeting in list:
         silence_time = 0
         speaking_time = 0
@@ -49,26 +51,32 @@ def main():
         sentence_length = 0
         current_start = 0
         prev_end = 0
+        prev_time_per_speaker = {}
+
+        largest_end_time = 0
 
         for line in meeting:
             sp = line[1]
-            start = line[2]
-            dur = line[3]
+            start = float(line[2])
+            dur = float(line[3])
             end = start+dur
+
+            if(end > largest_end_time):
+                largest_end_time = end
 
             sentence_length += 1
 
-            if prev_sp == sp:
-
-            else:
+            #deal with overlap not breaking up sentence
+            if prev_sp != sp and start - prev_time_per_speaker[str(sp)] > sentence_break_time:
                 if not sentence_lengths.has_key(sentence_length):
                     sentence_lengths[str(sentence_length)] = 0
                 sentence_lengths[str(sentence_length)] += 1
                 sentence_length = 0
-
                 silence_time += start - prev_end #what about overlap?
 
-            prev_end = end
+            prev_time_per_speaker[str(sp)] = prev_end = end
+
+        for line in meeting:
 
 
 
