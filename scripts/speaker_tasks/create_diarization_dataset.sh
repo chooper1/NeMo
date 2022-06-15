5b08d58b3f4a1b2e87e503c818fba3e2e5a195c7
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-15 14:42:31 -0300

    Update create_diarization_dataset.sh

--- scripts/speaker_tasks/create_diarization_dataset.sh
+++ scripts/speaker_tasks/create_diarization_dataset.sh
@@ -3,4 +3,20 @@
 # Create manifest file with alignments
 python create_alignment_manifest.py --input_manifest_filepath /home/chooper/projects/datasets/LibriSpeech/dev_clean.json --base_alignment_path /home/chooper/projects/branch_nemo/NeMo/scripts/speaker_tasks/LibriSpeech_Alignments/ --dataset dev-clean --output_path dev-clean-align.json
 # Create diarization session
-python create_diarization_dataset_librispeech.py --input_manifest_filepath ./dev-clean-align.json --output_dir outputs --num_speakers 3 --session_length 300 --num_sessions 1
+python create_diarization_dataset_librispeech.py \
+  --input_manifest_filepath ./dev-clean-align.json \
+  --output_dir outputs \
+  --output_filename diarization_session \
+  --num_sessions 1 \
+  --num_speakers 3 \
+  --session_length 300 \
+  --sentence_length_params [2.81, 0.1] \
+  --dominance_var 0.1 \
+  --min_dominance 0.05 \
+  --turn_prob 0.9 \
+  --mean_overlap 0.08 \
+  --mean_silence 0.08 \
+  --overlap_prob 0.3 \
+  --outputs rjc 
+
+# note: --enforce_num_speakers is store_true
