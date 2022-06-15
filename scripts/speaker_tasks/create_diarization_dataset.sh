f7bb7d934304b2ef2849d589a5de95c86837b0ce
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-15 14:48:11 -0300

    Update create_diarization_dataset.sh

--- scripts/speaker_tasks/create_diarization_dataset.sh
+++ scripts/speaker_tasks/create_diarization_dataset.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Create manifest file with alignments
-python create_alignment_manifest.py --input_manifest_filepath /home/chooper/projects/datasets/LibriSpeech/dev_clean.json --base_alignment_path /home/chooper/projects/branch_nemo/NeMo/scripts/speaker_tasks/LibriSpeech_Alignments/ --dataset dev-clean --output_path dev-clean-align.json
+python create_alignment_manifest.py \
+  --input_manifest_filepath /home/chooper/projects/datasets/LibriSpeech/dev_clean.json \
+  --base_alignment_path /home/chooper/projects/branch_nemo/NeMo/scripts/speaker_tasks/LibriSpeech_Alignments/ \
+  --dataset dev-clean \
+  --output_path dev-clean-align.json
 # Create diarization session
 python create_diarization_dataset_librispeech.py \
   --input_manifest_filepath ./dev-clean-align.json \
