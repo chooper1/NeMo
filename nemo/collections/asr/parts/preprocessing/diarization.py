35c77e46f2343bce6e5911a46bf4bf71bad547dc
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-16 03:22:35 -0300

    Update diarization.py

--- nemo/collections/asr/parts/preprocessing/diarization.py
+++ nemo/collections/asr/parts/preprocessing/diarization.py
@@ -353,7 +353,8 @@ class LibriSpeechGenerator(object):
                 sl = np.random.negative_binomial(
                     self._params.data_simulator.sentence_length_params[0], self._params.data_simulator.sentence_length_params[1]
                 )
-                print(sl)
+                if sl == 0:
+                    sl = 1
                 max_sentence_duration_sr = session_length_sr - running_length_sr
 
                 # only add if remaining length > 0.5 second
