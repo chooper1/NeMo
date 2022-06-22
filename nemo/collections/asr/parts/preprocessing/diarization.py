5b98ed7cc7551712b7d43e3f8a9425457eec70f4
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-06-22 02:05:09 -0300

    Update diarization.py

--- nemo/collections/asr/parts/preprocessing/diarization.py
+++ nemo/collections/asr/parts/preprocessing/diarization.py
@@ -270,7 +270,7 @@ class LibriSpeechGenerator(object):
         if prev_speaker != speaker_turn and prev_speaker != None and np.random.uniform(0, 1) < overlap_prob:
             overlap_percent = halfnorm(loc=0, scale=mean_overlap_percent*np.sqrt(np.pi)/np.sqrt(2)).rvs()
             desired_overlap_amount = int(prev_length_sr * overlap_percent)
-            self._desired_overlap_amount += int(length * mean_overlap_percent)
+            self._desired_overlap_amount += int(prev_length_sr * mean_overlap_percent)
             self._desired_overlap += mean_overlap_percent
             new_start = start - desired_overlap_amount
 
@@ -321,7 +321,7 @@ class LibriSpeechGenerator(object):
             if overlap_amount < desired_overlap_amount:
                 self._missing_overlap += desired_overlap_amount - overlap_amount
 
-            self._speaking_time -= overlap_amount
+            # self._speaking_time -= overlap_amount
 
             self._overlap_amount += overlap_amount
 
