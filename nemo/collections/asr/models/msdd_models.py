9c01f36599ae2ea28eedd9a4278faa65546be24d
chooper1 <chooper@college.harvard.edu>
2022-08-13 06:07:26 -0700

    Modified DL

--- nemo/collections/asr/models/msdd_models.py
+++ nemo/collections/asr/models/msdd_models.py
@@ -1723,3 +1723,302 @@ class OverlapAwareDiarizer:
                 self.AUDIO_RTTM_MAP, all_reference, all_hypothesis, collar=collar, ignore_overlap=ignore_overlap,
             )
         logging.info(f"  \n")
+
+class NeuralDiarizer:
+    """Class for inference based on multiscale diarization decoder (MSDD)."""
+
+    def __init__(self, cfg: DictConfig):
+        """ """
+        self._cfg = cfg
+        # Initialize diarization decoder
+        msdd_dict, spk_emb_dict = {}, {}
+        loaded = torch.load(cfg.diarizer.msdd_model.model_path)
+        _loaded = copy.deepcopy(loaded)
+        for name, param in _loaded['state_dict'].items():
+            if 'msdd._speaker_model' in name:
+                del loaded['state_dict'][name]
+                spk_emb_dict[name] = param
+            elif 'msdd.' in name:
+                msdd_dict[name] = param
+        new_model_path = cfg.diarizer.msdd_model.model_path.replace('.ckpt', '_msdd.ckpt')
+        torch.save(loaded, new_model_path)
+        cfg.diarizer.msdd_model.model_path = new_model_path
+        self.msdd_model = self._init_msdd_model(cfg)
+        pretrained_dict = self.msdd_model.state_dict()
+        pretrained_dict.update(msdd_dict)
+        self.msdd_model.load_state_dict(pretrained_dict)
+        self.diar_window_length = cfg.diarizer.msdd_model.parameters.diar_window_length
+        self.msdd_model.cfg = self.transfer_diar_params_to_model_params(self.msdd_model, cfg)
+        self.msdd_model.run_clus_from_loaded_emb = True
+        self.manifest_filepath = self.msdd_model.cfg.test_ds.manifest_filepath
+        self.use_clus = False
+        self.use_dec = True
+        self.AUDIO_RTTM_MAP = audio_rttm_map(self.manifest_filepath)
+
+        # Initialize clustering and embedding preparation instance (as a diarization encoder).
+        self.clustering_embedding = ClusterEmbedding(cfg_base=cfg, cfg_msdd_model=self.msdd_model.cfg)
+        self.clustering_embedding.run_clus_from_loaded_emb = True
+
+        self.use_prime_cluster_avg_emb = True
+        self.max_pred_length = 0
+        self.eps = 10e-5
+
+    def transfer_diar_params_to_model_params(self, msdd_model, cfg):
+        msdd_model.cfg.base.diarizer.out_dir = cfg.diarizer.out_dir
+        msdd_model.cfg.test_ds.manifest_filepath = cfg.diarizer.manifest_filepath
+        msdd_model.cfg.test_ds.emb_dir = cfg.diarizer.out_dir
+        msdd_model.cfg_base = cfg
+        msdd_model._cfg.base.diarizer.clustering.parameters.max_num_speakers = (
+            cfg.diarizer.clustering.parameters.max_num_speakers
+        )
+        return msdd_model.cfg
+
+    def _init_msdd_model(self, cfg):
+        self.device = 'cuda'
+        if not torch.cuda.is_available():
+            self.device = 'cpu'
+            logging.warning(
+                "Running model on CPU, for faster performance it is adviced to use atleast one NVIDIA GPUs"
+            )
+
+        if cfg.diarizer.msdd_model.model_path.endswith('.nemo'):
+            logging.info(f"Using local speaker model from {cfg.diarizer.msdd_model.model_path}")
+            msdd_model = EncDecDiarLabelModel.restore_from(restore_path=cfg.diarizer.msdd_model.model_path)
+        elif cfg.diarizer.msdd_model.model_path.endswith('.ckpt'):
+            msdd_model = EncDecDiarLabelModel.load_from_checkpoint(checkpoint_path=cfg.diarizer.msdd_model.model_path)
+        return msdd_model
+
+    def get_pred_mat(self, data_list):
+        all_tups = tuple()
+        for data in data_list:
+            all_tups += data[0]
+        n_est_spks = len(set(all_tups))
+        digit_map = dict(zip(sorted(set(all_tups)), range(n_est_spks)))
+        total_len = max([sess[1].shape[1] for sess in data_list])
+        sum_pred = torch.zeros(total_len, n_est_spks)
+        for data in data_list:
+            _dim_tup, pred_mat = data[:2]
+            dim_tup = [digit_map[x] for x in _dim_tup]
+            if len(pred_mat.shape) == 3:
+                pred_mat = pred_mat.squeeze(0)
+            if n_est_spks in [1, 2]:
+                sum_pred = pred_mat
+            else:
+                _end = pred_mat.shape[0]
+                sum_pred[:_end, dim_tup] += pred_mat.cpu().float()
+        sum_pred = sum_pred / (n_est_spks - 1)
+        return sum_pred
+
+    def get_integrated_preds_list(self, uniq_id_list, test_data_collection, preds_list):
+        """
+        Merge multiple sequence inference outputs into a session level result.
+        """
+        session_dict = get_id_tup_dict(uniq_id_list, test_data_collection, preds_list)
+        output_dict = {uniq_id: [] for uniq_id in uniq_id_list}
+        for uniq_id, data_list in session_dict.items():
+            sum_pred = self.get_pred_mat(data_list)
+            output_dict[uniq_id] = sum_pred.unsqueeze(0)
+        output_list = [output_dict[uniq_id] for uniq_id in uniq_id_list]
+        return output_list
+
+    def diarize(self):
+        """
+        Launch diarization pipeline which starts from VAD (or a oracle VAD stamp generation), initialization clustering and
+        multiscale diarization decoder (MSDD). Note that the result of MSDD can include multiple speakers at the same time.
+        Therefore, RTTM output of MSDD needs to be based on make_rttm_with_overlap() function that can generate overlapping
+        timestamps.
+        """
+        torch.set_grad_enabled(False)
+
+        self.clustering_embedding.prepare_cluster_embs_infer()
+        self.msdd_model.pairwise_infer = True
+        self.msdd_model.get_emb_clus_infer(self.clustering_embedding)
+        preds_list, targets_list, signal_lengths_list = self.run_pairwise_diarization()
+        for threshold in list(self._cfg.diarizer.msdd_model.parameters.sigmoid_threshold):
+            self.run_overlap_aware_eval(preds_list, threshold)
+
+    def get_range_average(self, signals, emb_vectors, diar_window_index, test_data_collection):
+        emb_vectors_split = torch.zeros_like(emb_vectors)
+        uniq_id = os.path.splitext(os.path.basename(test_data_collection.rttm_file))[0]
+        clus_label_tensor = torch.tensor([x[-1] for x in self.msdd_model.clus_test_label_dict[uniq_id]])
+        for spk_idx in range(len(test_data_collection.target_spks)):
+            stt, end = (
+                diar_window_index * self.diar_window_length,
+                min((diar_window_index + 1) * self.diar_window_length, clus_label_tensor.shape[0]),
+            )
+            seq_len = end - stt
+            if stt < clus_label_tensor.shape[0]:
+                target_clus_label_tensor = clus_label_tensor[stt:end]
+                emb_seq, seg_length = (
+                    signals[stt:end, :, :],
+                    min(
+                        self.diar_window_length,
+                        clus_label_tensor.shape[0] - diar_window_index * self.diar_window_length,
+                    ),
+                )
+                target_clus_label_bool = target_clus_label_tensor == test_data_collection.target_spks[spk_idx]
+
+                # There are cases where there is no corresponding speaker in split range, so any(target_clus_label_bool) could be False.
+                if any(target_clus_label_bool) == True:
+                    emb_vectors_split[:, :, spk_idx] = torch.mean(emb_seq[target_clus_label_bool], dim=0)
+
+                # In case when the loop reaches the end of the sequence
+                if seq_len < self.diar_window_length:
+                    emb_seq = torch.cat(
+                        [
+                            emb_seq,
+                            torch.zeros(self.diar_window_length - seq_len, emb_seq.shape[1], emb_seq.shape[2]).to(
+                                self.device
+                            ),
+                        ],
+                        dim=0,
+                    )
+            else:
+                emb_seq = torch.zeros(self.diar_window_length, emb_vectors.shape[0], emb_vectors.shape[1]).to(
+                    self.device
+                )
+                seq_len = 0
+        return emb_vectors_split, emb_seq, seq_len
+
+    def get_range_clus_avg_emb(self, test_batch, _test_data_collection, split_count):
+        """
+        split_count
+        """
+        _signals, signal_lengths, _targets, _emb_vectors = test_batch
+        sess_emb_vectors, sess_emb_seq, sess_sig_lengths = [], [], []
+        split_count = torch.ceil(torch.tensor(_signals.shape[1] / self.diar_window_length)).int()
+        self.max_pred_length = max(self.max_pred_length, self.diar_window_length * split_count)
+        for k in range(_signals.shape[0]):
+            signals, emb_vectors, test_data_collection = _signals[k], _emb_vectors[k], _test_data_collection[k]
+            for diar_window_index in range(split_count):
+                emb_vectors_split, emb_seq, seq_len = self.get_range_average(
+                    signals, emb_vectors, diar_window_index, test_data_collection
+                )
+                sess_emb_vectors.append(emb_vectors_split)
+                sess_emb_seq.append(emb_seq)
+                sess_sig_lengths.append(seq_len)
+        sess_emb_vectors = torch.stack(sess_emb_vectors)
+        sess_emb_seq = torch.stack(sess_emb_seq)
+        sess_sig_lengths = torch.tensor(sess_sig_lengths)
+        return sess_emb_vectors, sess_emb_seq, sess_sig_lengths
+
+    def diar_infer(self, test_batch, test_data_collection):
+        """
+        Launch forward_infer() function by feeding the session-wise embedding sequences to get pairwise speaker prediction values.
+        If split_infer is True, the input audio clips are broken into short sequences then cluster average embeddings are calculated
+        for inference.
+        If split_infer is True, the input embedding sequence is broken down into short sequences to calculate the cluster-average vectors.
+        Split-infer might result in an improved results if calculating clustering average on the shorter timespan can help speaker assignment.
+        Args:
+            test_batch: (list)
+                List containing embedding sequences, length of embedding sequences, ground truth labels (if exists) and initializing embedding vectors.
+            test_data_collection: (list)
+                List containing test-set dataloader contents. test_data_collection includes wav file path, RTTM file path, clustered speaker indices.
+        Returns:
+            preds: (torch.Tensor)
+            targets: (torch.Tensor)
+            signal_lengths: (torch.Tensor)
+        """
+        signals, signal_lengths, _targets, emb_vectors = test_batch
+        if self._cfg.diarizer.msdd_model.parameters.split_infer:
+            split_count = torch.ceil(torch.tensor(signals.shape[1] / self.diar_window_length)).int()
+            sess_emb_vectors, sess_emb_seq, sess_sig_lengths = self.get_range_clus_avg_emb(
+                test_batch, test_data_collection, split_count
+            )
+            if self._cfg.diarizer.msdd_model.parameters.use_longest_scale_clus_avg_emb:
+                sess_emb_vectors = self.msdd_model.get_longest_scale_clus_avg_emb(sess_emb_vectors)
+            with autocast():
+                _preds, scale_weights = self.msdd_model.forward_infer(
+                    input_signal=sess_emb_seq,
+                    input_signal_length=sess_sig_lengths,
+                    emb_vectors=sess_emb_vectors,
+                    targets=None,
+                )
+            _preds = _preds.reshape(len(signal_lengths), split_count * self.diar_window_length, -1)
+            _preds = _preds[:, : signals.shape[1], :]
+        else:
+            if self._cfg.diarizer.msdd_model.parameters.use_longest_scale_clus_avg_emb:
+                sess_emb_vectors = self.msdd_model.get_longest_scale_clus_avg_emb(sess_emb_vectors)
+            with autocast():
+                _preds, scale_weights = self.msdd_model.forward_infer(
+                    input_signal=signals, input_signal_length=signal_lengths, emb_vectors=emb_vectors, targets=_targets
+                )
+        self.max_pred_length = max(_preds.shape[1], self.max_pred_length)
+        preds = torch.zeros(_preds.shape[0], self.max_pred_length, _preds.shape[2])
+        targets = torch.zeros(_preds.shape[0], self.max_pred_length, _preds.shape[2])
+        preds[:, : _preds.shape[1], :] = _preds
+        return preds, targets, signal_lengths
+
+    def run_pairwise_diarization(self, embedding_dir='./', device='cuda'):
+        """
+        Setup the parameters needed for batch inference and run batch inference. Note that each sample is pairwise speaker input.
+        The pairwise inference results are reconstructed to make session-wise prediction results.
+        Args:
+            embedding_dir: (str)
+                Embedding directory in string format
+            device: (torch.device)
+                Torch device variable.
+        Returns:
+            integrated_preds_list: (list)
+                List containing the session-wise speaker predictions in torch.tensor format.
+            targets_list: (list)
+                List containing the ground-truth labels in matrix format filled with  0 or 1.
+            signal_lengths_list: (list)
+                List containing the actual length of each sequence in session.
+        """
+        test_cfg = self.msdd_model.cfg.test_ds
+        self.msdd_model.setup_test_data(test_cfg)
+        self.msdd_model = self.msdd_model.to(self.device)
+        self.msdd_model.eval()
+        torch.set_grad_enabled(False)
+        cumul_sample_count = [0]
+        preds_list, targets_list, signal_lengths_list = [], [], []
+        uniq_id_list = get_uniq_id_list_from_manifest(self.manifest_filepath)
+        test_data_collection = [d for d in self.msdd_model.data_collection]
+        for sidx, test_batch in enumerate(tqdm(self.msdd_model.test_dataloader())):
+            test_batch = [x.to(self.device) for x in test_batch]
+            signals, signal_lengths, _targets, emb_vectors = test_batch
+            cumul_sample_count.append(cumul_sample_count[-1] + signal_lengths.shape[0])
+            preds, targets, signal_lengths = self.diar_infer(
+                test_batch, test_data_collection[cumul_sample_count[-2] : cumul_sample_count[-1]]
+            )
+            if self._cfg.diarizer.msdd_model.parameters.seq_eval_mode:
+                self.msdd_model._accuracy_test(preds, targets, signal_lengths)
+
+            preds_list.extend(list(torch.split(preds, 1)))
+            targets_list.extend(list(torch.split(targets, 1)))
+            signal_lengths_list.extend(list(torch.split(signal_lengths, 1)))
+
+        if self._cfg.diarizer.msdd_model.parameters.seq_eval_mode:
+            f1_score, simple_acc = compute_accuracies(self.msdd_model)
+            logging.info(f"Test Inference F1 score. {f1_score:.4f}, simple Acc. {simple_acc:.4f}")
+        integrated_preds_list = self.get_integrated_preds_list(uniq_id_list, test_data_collection, preds_list)
+        return integrated_preds_list, targets_list, signal_lengths_list
+
+    def run_overlap_aware_eval(self, preds_list: List[torch.Tensor], threshold: float):
+        """
+        Based on the predicted sigmoid values, render RTTM files then evaluate the overlap-aware diarization results.
+        Args:
+            preds_list: (list)
+                List containing predicted pairwise speaker labels.
+            threshold: (float)
+                A floating-point threshold value that determines overlapped speech detection.
+                    - If threshold is 1.0, no overlap speech is detected and only detect major speaker.
+                    - If threshold is 0.0, all speakers are considered active at any time step.
+        """
+        for k, (collar, ignore_overlap) in enumerate([(0.25, True), (0.0, False)]):
+            logging.info(
+                f"     [Threshold: {threshold:.4f}]   [infer_overlap={not ignore_overlap}]   [use_clus={self.use_clus}]    [use_dec={self.use_dec}]"
+            )
+            all_reference, all_hypothesis = make_rttm_with_overlap(
+                self.manifest_filepath,
+                self.msdd_model.clus_test_label_dict,
+                preds_list,
+                threshold=threshold,
+                infer_overlap=(not ignore_overlap),
+                use_clus=self.use_clus,
+                use_dec=self.use_dec,
+            )
+            score = score_labels(
+                self.AUDIO_RTTM_MAP, all_reference, all_hypothesis, collar=collar, ignore_overlap=ignore_overlap,
+            )
