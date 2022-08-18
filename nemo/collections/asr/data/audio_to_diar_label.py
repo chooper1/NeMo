1a6efb28fea74874056502c32941301c3646ced4
chooper1 <54963573+chooper1@users.noreply.github.com>
2022-08-18 17:53:49 -0300

    Update audio_to_diar_label.py

--- nemo/collections/asr/data/audio_to_diar_label.py
+++ nemo/collections/asr/data/audio_to_diar_label.py
@@ -879,7 +879,7 @@ class SyntheticDataLoader(torch.utils.data.dataloader.DataLoader):
     """
     def __init__(self, *args, **kwargs):
         if kwargs['dataset'].global_rank == 0:   #remove for working version
-            logging.info(f"Reloading dataset in synthetic dataloader, rank is {kwargs['dataset'].trainer.global_rank}")
+            logging.info(f"Reloading dataset in synthetic dataloader, rank is {kwargs['dataset'].global_rank}")
             kwargs['dataset'].regenerate_dataset()
         super().__init__(*args, **kwargs)
 
